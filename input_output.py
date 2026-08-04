0488541d481d75b64400aff9c45c1aa12dd91cd4
Tanishka Chandra <ms.tanishkachandra@gmail.com>
2026-08-04 22:19:13 +0530

    Add comments to input_output.py for clarity
    Added comments to clarify the purpose of print statements.

---
+++ input_output.py
@@ -0,0 +1,6 @@
+name=input("Enter name:")) #takes string type input
+age=int(input("Enter age:")) #takes integer type input
+height=float(input("Enter height:")) #takes float type input
+print("Name:",name) #display name
+print("Age:",age) #display age
+print("Height:",height) #display height

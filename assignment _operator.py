62d6538b6642d50657d84c8e9da3ce99f9772a3a
Tanishka Chandra <ms.tanishkachandra@gmail.com>
2026-08-08 22:22:21 +0530

    Add assignment operator program
    Demonstrate various assignment operators in Python.

---
+++ assignment _operator.py
@@ -0,0 +1,16 @@
+a = int(input("Enter a number: ")
+print("Initial value:", a)
+a += 5
+print("After += 5:", a)
+a -= 2
+print("After -= 2:", a)
+a *= 3
+print("After *= 3:", a)
+a /= 2
+print("After /= 2:", a)
+a //= 2
+print("After //= 2:", a)
+a %= 3
+print("After %= 3:", a)
+a **= 2
+print("After **= 2:", a)

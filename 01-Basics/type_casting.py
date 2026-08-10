8bbf13c60eb7e161bfbf8738eb0cca499bfa434b
Tanishka Chandra <ms.tanishkachandra@gmail.com>
2026-08-10 21:49:55 +0530

    Add Python Basics Program

---
+++ 01-Basics/type_casting.py
@@ -0,0 +1,6 @@
+num=25
+print(type(num))  #integer
+
+numf=float(num)
+print(numf)
+print(type(numf))  #float
---
+++ 01-Basics/variable.py
@@ -0,0 +1,7 @@
+name="Tanishka" #string
+age=20 #integer
+height=5.2 #float
+
+print("Name:",name)
+print("Age:",age)
+print("Height:",height)

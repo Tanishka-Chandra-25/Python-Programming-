f37b26a4fb10dd19e49eb9360ead1d4a28e5b68b
Tanishka Chandra <ms.tanishkachandra@gmail.com>
2026-08-13 23:36:22 +0530

    Add sum and average marks calculation program

---
+++ 01-Basics/sum_average_marks.py
@@ -0,0 +1,9 @@
+m1=int(input("Enter marks of subject 1:"))
+m2=int(input("Enter marks of subject 2:"))
+m3=int(input("Enter marks of subject 3:"))
+m4=int(input("Enter marks of subject 4:"))
+m5=int(input("Enter marks of subject 5:"))
+s=m1+m2+m3+m4+m5
+avg=s/5
+print("The sum of all marks:",s)
+print("The average of total marks:",avg)

80c5f9b02723720975a3c7a9cd6b75b635c35f91
Tanishka Chandra <ms.tanishkachandra@gmail.com>
2026-08-15 15:26:23 +0530

    Delete input_output.py program file

--- input_output.py
+++
@@ -1,6 +0,0 @@
-name=input("Enter name:")) #takes string type input
-age=int(input("Enter age:")) #takes integer type input
-height=float(input("Enter height:")) #takes float type input
-print("Name:",name) #display name
-print("Age:",age) #display age
-print("Height:",height) #display height

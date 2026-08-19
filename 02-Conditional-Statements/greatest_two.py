899e82110d2c9a7d9f8d6af0ec1548c83b73ee98
Tanishka Chandra <ms.tanishkachandra@gmail.com>
2026-08-19 23:35:27 +0530

    Add find the greatest of two numbers program

---
+++ 02-Conditional-Statements/greatest_two.py
@@ -0,0 +1,9 @@
+num1=int(input("Enter number 1:"))
+num2=int(input("Enter number 2:"))
+if(num1>num2):
+  print("Number 1 is greatest")
+elif(num2>num1):
+  print("Number 2 is greatest")
+else:
+  print("Both are equal")
+               
